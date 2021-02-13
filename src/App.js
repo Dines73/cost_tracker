d98a85438dd25f30b00e424cef797f9bd8341b7f
edin <edo.truman@gmail.com>
2021-02-13 15:18:37 +0100

    Button bug reveal

--- src/App.js
+++ src/App.js
@@ -9,27 +9,29 @@ const App = () => {
   const classes = useStyles()
 
   return (
-    <Grid
-      className={classes.grid}
-      container
-      spacing={0}
-      alignItems="center"
-      justify="center"
-      style={{ height: "100vh" }}
-    >
-      <Grid item xs={12} sm={4}>
-        <Details title="Income" />
-      </Grid>
-      <Grid item xs={12} sm={3}>
-        <Main />
-      </Grid>
-      <Grid item xs={12} sm={4}>
-        <Details title="Expense" />
+    <div>
+      <Grid
+        className={classes.grid}
+        container
+        spacing={0}
+        alignItems="center"
+        justify="center"
+        style={{ height: "100vh" }}
+      >
+        <Grid item xs={12} sm={4}>
+          <Details title="Income" />
+        </Grid>
+        <Grid item xs={12} sm={3}>
+          <Main />
+        </Grid>
+        <Grid item xs={12} sm={4}>
+          <Details title="Expense" />
+        </Grid>
       </Grid>
       <PushToTalkButtonContainer>
         <PushToTalkButton />
       </PushToTalkButtonContainer>
-    </Grid>
+    </div>
   )
 }
 
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react"
 import ReactDOM from "react-dom"
 import { Provider } from "./context/context"
-import { SpeechProvider } from "@sppechly/react-client"
+import { SpeechProvider } from "@speechly/react-client"
 
 import App from "./App"
 import "./index.css"
